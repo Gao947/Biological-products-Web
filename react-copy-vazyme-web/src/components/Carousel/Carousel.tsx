311590cf63236d4c71dedbee7191b859ee641c30
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-03-23 22:39:01 +1100

    FIX： Carousel img

--- react-copy-vazyme-web/src/components/Carousel/Carousel.tsx
+++ react-copy-vazyme-web/src/components/Carousel/Carousel.tsx
@@ -1,4 +1,7 @@
 import Carousel from 'react-bootstrap/Carousel';
+import carousel1 from "../../img/Carousel1.webp";
+import carousel2 from "../../img/carousel2.webp";
+import carousel3 from "../../img/carousel3.webp";
 
 function CarouselComponent() {
   return (
@@ -6,39 +9,23 @@ function CarouselComponent() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          src={carousel1}
           alt="First slide"
         />
-        {/* <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          src={carousel2}
           alt="Second slide"
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          src={carousel3}
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
