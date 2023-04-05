d7add29a0ed05ed3185a48fb40828159c5973f10
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-04-05 22:36:03 +1000

    WIP: BOOK Component

--- react-copy-vazyme-web/src/components/Book/Book.tsx
+++ react-copy-vazyme-web/src/components/Book/Book.tsx
@@ -1,8 +1,13 @@
 import styles from "./Book.module.css";
+import Image from 'react-bootstrap/Image';
+
+import page from "../../img/book.jpg";
 
 function BookComponent() {
-    return(
-        <div>book part</div>
+    return (
+        <div>
+            <Image className={styles["page"]} src={page} />
+        </div>
     )
 }
 
